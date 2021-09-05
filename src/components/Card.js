d7b250c0370c93b684562b053b5ebbcc44ecbd09
Olga T <jenflower97@gmail.com>
2021-09-05 23:42:30 +0400

    using user context for visible delete button

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,17 @@
+import React from 'react'
+import { CurrentUserContext } from '../contexts/CurrentUserContext'
+
 export default function Card(props) {
+    const currentUser = React.useContext(CurrentUserContext)
+    // console.log('user id', currentUser._id)
+    const isOwn = props.card.owner._id === currentUser._id;
+    console.log('props.card.owner._id', props.card.owner._id)
+    const cardDeleteButtonClassName = (
+        `${isOwn ? 'card__trush' : 'card__trush-hidden'}`
+      ); 
     return (
         <li className="card">
-            <button className="card__trush card__trush-hidden" type="submit" aria-label="Delete"/>
+            <button className={`${cardDeleteButtonClassName}`} type="submit" aria-label="Delete"/>
             <img className="card__image" src={props.card.link} onClick={props.onClick} alt={props.card.name}/>
             <div className="card__content">
                 <h2 className="card__title">{props.card.name}</h2>
